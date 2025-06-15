1368a8edd0608b9f014777b497a48dda3685dba8
Naveen949031 <yennamsettinaveen1124@gmail.com>
2025-06-15 13:30:29 +0530

    Create APSCHE.pl

---
+++ APSCHE.pl
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import PIL. Image as Image
+import cv2
+import os
+import numpy as np
+import pathlib
+data_dir = "..C:\Users\raman\Downloads\archive\Rice_Image_Dataset"
+data_dir = pathlib. Path(data_dir)
+data_dir
